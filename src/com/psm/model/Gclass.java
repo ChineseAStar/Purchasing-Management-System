922153ff124b9b5bf5dea980354c10c26f37036a
彩云鹏 <1254074046@qq.com>
2018-01-04 10:31:06 +0800

    Add files via upload

---
+++ src/com/psm/model/Gclass.java
@@ -0,0 +1,41 @@
+package com.psm.model;
+
+public class Gclass {
+
+	private String daid;
+	private String zhongid;
+	private String xiaoid;
+	private String name;
+	private int type;
+	public String getDaid() {
+		return daid;
+	}
+	public void setDaid(String daid) {
+		this.daid = daid;
+	}
+	public String getZhongid() {
+		return zhongid;
+	}
+	public void setZhongid(String zhongid) {
+		this.zhongid = zhongid;
+	}
+	public String getXiaoid() {
+		return xiaoid;
+	}
+	public void setXiaoid(String xiaoid) {
+		this.xiaoid = xiaoid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	
+}
