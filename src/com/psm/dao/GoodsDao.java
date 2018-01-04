461fde93faed02b64f20c958ef7a8cb5254cf442
彩云鹏 <1254074046@qq.com>
2018-01-04 10:29:17 +0800

    Add files via upload

--- src/com/psm/dao/GoodsDao.java
+++ src/com/psm/dao/GoodsDao.java
@@ -3,6 +3,7 @@ package com.psm.dao;
 import java.util.List;
 
 import com.psm.common.Page;
+import com.psm.model.Gclass;
 import com.psm.model.Goods;
 import com.psm.model.GoodsMini;
 import com.psm.model.GoodsView;
@@ -14,8 +15,14 @@ public interface GoodsDao {
 	List<GoodsView> getPageInfo(Page pages);
 	int update(Goods good);//更新商品信息
 	int del(String id);//删除商品信息
-	int add(Goods good);//添加商品信息
+	int add(String id,String name,String sid);//添加商品信息
 	List<String> findSid();//查找供应商编号
 	List<GoodsMini> findGid();
+	List<Gclass> clist();
+	int addClass(String daid,String zhongid,String xiaoid,String name,int type);
+	int delClass(String daid,String zhongid,String xiaoid);
+	int findClass(String daid,String zhongid,String xiaoid);
+	String findgid(String gid);
+	int checkdel(String daid,String zhongid,String xiaoid);
 	
 }
